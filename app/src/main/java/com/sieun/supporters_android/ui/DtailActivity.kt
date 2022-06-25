7c627bc0efe27b3a631c6d29fc6ee0485325a4ac
JJY <juyoung0520@naver.com>
2022-06-25 16:15:36 +0900

    chore: 프로젝트 세팅
    - 엑티비티, 뷰모델 생성
    - 데이터바인딩 설정

---
+++ app/src/main/java/com/sieun/supporters_android/ui/DtailActivity.kt
@@ -0,0 +1,20 @@
+package com.sieun.supporters_android.ui
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.sieun.supporters_android.R
+import com.sieun.supporters_android.databinding.ActivityDetailBinding
+
+class DtailActivity : AppCompatActivity() {
+    private val viewModel: DetailViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding =
+            DataBindingUtil.setContentView<ActivityDetailBinding>(this, R.layout.activity_detail)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+    }
+}
---
+++ app/src/main/java/com/sieun/supporters_android/ui/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.sieun.supporters_android.ui
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel(){
+}
