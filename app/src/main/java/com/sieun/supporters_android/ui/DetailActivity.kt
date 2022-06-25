e0bd89b37925c1c7f3fd4cf38ccc662f95a8837f
JJY <juyoung0520@naver.com>
2022-06-26 06:21:07 +0900

    fix banner, detail activity

--- app/src/main/java/com/sieun/supporters_android/ui/DetailActivity.kt
+++ app/src/main/java/com/sieun/supporters_android/ui/DetailActivity.kt
@@ -9,33 +9,40 @@ import com.sieun.supporters_android.R
 import com.sieun.supporters_android.databinding.ActivityBannerBinding
 import com.sieun.supporters_android.databinding.ActivityDetailBinding
 import com.sieun.supporters_android.model.CategoryItem
+import com.sieun.supporters_android.ui.list.CategoryItemViewHolder.Companion.ITEM_ID
+import com.sieun.supporters_android.ui.list.CategoryListAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailActivity : AppCompatActivity() {
     private val viewModel: DetailViewModel by viewModels()
-    private lateinit var tagAdapter: TagAdapter
+    private val tagAdapter: TagAdapter by lazy { TagAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding =
             DataBindingUtil.setContentView<ActivityDetailBinding>(this, R.layout.activity_detail)
         binding.vm = viewModel
+        binding.activity = this
         binding.lifecycleOwner = this
 
         intent.extras?.let {
-            //val id = intent.getLongExtra()
-            //viewModel.getDetailItem()
+            val id = intent.getLongExtra(ITEM_ID, 0L)
+            viewModel.getDetailItem(id)
         }
-        viewModel.getDetailItem(1)
-        onClickButton(CategoryItem(1, "d", "d", "https://www.unhcr.or.kr/unhcr/program/donate_row/support01.jsp", ""))
+
+        initView(binding)
     }
 
     private fun initView(binding: ActivityDetailBinding) = with(binding) {
         detailTagRv.adapter = tagAdapter
+
+        detailBackIv.setOnClickListener {
+            this@DetailActivity.finish()
+        }
     }
 
-    private fun onClickButton(item: CategoryItem) {
+    fun onClickButton(item: CategoryItem) {
         Intent(this, WebActivity::class.java)
             .putExtra(SITE_URL, item.siteUrl)
             .let { intent -> startActivity(intent) }
