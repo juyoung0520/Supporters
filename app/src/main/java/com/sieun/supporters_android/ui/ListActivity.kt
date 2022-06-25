75c49561cda056afb20c26f947c91f3b64e0b90c
Greenddoovie <75981415+Greenddoovie@users.noreply.github.com>
2022-06-26 04:04:38 +0900

    modify: adpater 생성 위치 변경 및 변수 추가 feat: viewholder arrow 동작 추가

--- app/src/main/java/com/sieun/supporters_android/ui/ListActivity.kt
+++ app/src/main/java/com/sieun/supporters_android/ui/ListActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.sieun.supporters_android.R
 import com.sieun.supporters_android.databinding.ActivityListBinding
+import com.sieun.supporters_android.ui.list.CategoryListAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +18,7 @@ class ListActivity: AppCompatActivity() {
     private var categoryNum = 0L
     private lateinit var binding: ActivityListBinding
     private lateinit var categoryName: String
+    private val categoryListAdapter by lazy { CategoryListAdapter(categoryName) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +30,7 @@ class ListActivity: AppCompatActivity() {
         processIntent()
         fetchCategoryItems(categoryNum)
         setBackButton()
+        setRVSetting()
     }
 
     private fun fetchCategoryItems(categoryNum: Long) {
@@ -46,6 +50,13 @@ class ListActivity: AppCompatActivity() {
             Glide.with(binding.root)
                 .load(it.getString(HomeActivity.CATEGORY_THUMBNAIL))
                 .into(binding.categoryThumbnail)
+        }
+    }
+
+    private fun setRVSetting() {
+        binding.listContainer.infiniteList.apply {
+            adapter = categoryListAdapter
+            layoutManager = LinearLayoutManager(this.context)
         }
     }
 
--- app/src/main/java/com/sieun/supporters_android/ui/list/util.kt
+++ app/src/main/java/com/sieun/supporters_android/ui/list/util.kt
@@ -1,15 +1,10 @@
 package com.sieun.supporters_android.ui.list
 
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sieun.supporters_android.model.CategoryItemsResult
 
 @BindingAdapter("bind_categorylist")
 fun bindCategoryList(recyclerView: RecyclerView, list: CategoryItemsResult) {
-    if (recyclerView.adapter == null) {
-        recyclerView.adapter = CategoryListAdapter()
-        recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
-    }
     (recyclerView.adapter as CategoryListAdapter).submitList(list.items)
 }
